ff3f3f42ab44d8d88a16f14767edaf8588404696
vipulchaudhary16 <vipulr6111@gmail.com>
2022-01-13 15:31:24 +0530

    user login and signup page ready

--- app/src/main/java/com/veercreation/twitttwitt/MainActivity.java
+++ app/src/main/java/com/veercreation/twitttwitt/MainActivity.java
@@ -2,9 +2,15 @@ package com.veercreation.twitttwitt;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.parse.ParseAnalytics;
+import com.veercreation.twitttwitt.user.LogInSignInActivity;
+
+import java.util.Objects;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,6 +19,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Timer timer = new Timer("Timer");;
+        timer.schedule(task , 1400L);
+
+        Objects.requireNonNull(getSupportActionBar()).hide();
+
         ParseAnalytics.trackAppOpenedInBackground(getIntent());
     }
+    TimerTask task = new TimerTask() {
+        public void run() {
+            Intent intent = new Intent(MainActivity.this, LogInSignInActivity.class);
+            startActivity(intent);
+            finish();
+        }
+    };
 }
